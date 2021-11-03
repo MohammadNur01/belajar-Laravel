53435b1124ae28491cc982f93d8345c10bd0fa62
MohammadNur01 <mohammadnurrossifumi@gmail.com>
2021-11-03 11:17:22 +0700

    add file

---
+++ resources/views/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layout/main')
+
+@section('title', 'MH Web')
+
+@section('container')
+<div class="container">
+  <div class="row">
+    <div class="col-10">
+      <h1 class="mt-4">Hello, </h1>
+
+    </div>
+  </div>
+</div>
+@endsection
