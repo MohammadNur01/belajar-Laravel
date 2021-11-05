361a97b846469382618f864ddbd2bf91bcc779b1
MohammadNur01 <mohammadnurrossifumi@gmail.com>
2021-11-05 09:50:46 +0700

    menggunakan eloquent pada tombol students

---
+++ resources/views/students/index.blade.php
@@ -0,0 +1,21 @@
+@extends('layout.main')
+
+@section('title', 'Daftar Mahasiswa')
+
+@section('container')
+<div class="container">
+  <div class="row">
+    <div class="col-6">
+      <h1 class="mt-4">Daftar Mahasiswa</h1>
+      <ul class="list-group">
+        @foreach($students as $student)
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+          {{$student->nama}}
+          <a href="" class="btn btn-info"> detail</a>
+        </li>
+        @endforeach
+      </ul>
+    </div>
+  </div>
+</div>
+@endsection
